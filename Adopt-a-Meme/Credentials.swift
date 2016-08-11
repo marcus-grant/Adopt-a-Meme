4f234a15120b68805905e52b0a4a3418cb2e3a77
Marcus Grant <marcusfg@gmail.com>
2016-08-11 19:11:10 -0400

    Initial Commit

---
+++ Adopt-a-Meme/Credentials.swift
@@ -0,0 +1,12 @@
+//
+//  Credentials.swift
+//  Adopt-a-Meme
+//
+//  Created by Marcus Grant on 8/11/16.
+//  Copyright © 2016 Marcus Grant. All rights reserved.
+//
+
+import Foundation
+
+let APISecret = 53a4abeb4b923e0f91632a701c01221dea4b935c
+let APIKey = 40cfe816d2b7d4246640c1a351b21772
---
+++ Adopt-a-Meme/PetFinderController.swift
@@ -0,0 +1,11 @@
+//
+//  PetFinderController.swift
+//  Adopt-a-Meme
+//
+//  Created by Marcus Grant on 8/11/16.
+//  Copyright © 2016 Marcus Grant. All rights reserved.
+//
+
+import Foundation
+
+
